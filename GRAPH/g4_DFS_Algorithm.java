9e50db01b8aa1b28a366147ebb628e53309ff885
Ravi B. Dhavare <100593775+ravidhavare111@users.noreply.github.com>
2023-12-03 16:06:16 +0530

    .

---
+++ GRAPH/g4_DFS_Algorithm.java
@@ -0,0 +1,49 @@
+import java.util.*;
+public class g4_DFS_Algorithm {
+
+    public static void DFS(int node, ArrayList<ArrayList<Integer>> graph, int visited[], int parent[] ){
+
+        // visted[node] = 1;
+
+        System.out.print(node+" ");
+
+        for(int x : graph.get(node)){
+
+            if(visited[x] == 0){
+                visited[x] = 1;
+
+                parent[x] = node;
+
+                DFS(x, graph, visited, parent);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int e = sc.nextInt();
+
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+        for(int i = 0 ; i<=n ; i++){
+            graph.add(new ArrayList<Integer>());
+        }
+
+        for(int i = 1 ; i<=e ; i++){
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        int visited[] = new int[n+1];
+        int parent[] = new int[n+1];
+
+        visited[1] = 1;
+
+        DFS(1, graph, visited, parent);
+    }
+    
+}
